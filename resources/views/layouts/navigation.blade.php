18215c579b47bc8ed20638b4803dea0546884075
m67pg <m67pg.2021@gmail.com>
2022-02-16 06:23:48 +0900

    no message

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,17 +5,12 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('/') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('project.index')" :active="request()->routeIs('project.index')">
                         {{ __('プロジェクト') }}
@@ -83,8 +78,23 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('project.index')" :active="request()->routeIs('project.index')">
+                {{ __('プロジェクト') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('crowd_sourcing.index')" :active="request()->routeIs('crowd_sourcing.index')">
+                {{ __('クラウドソーシング') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('orderer.index')" :active="request()->routeIs('orderer.index')">
+                {{ __('発注者') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('progress.index')" :active="request()->routeIs('progress.index')">
+                {{ __('進捗') }}
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+//Route::get('/', function () {
+//    return view('dashboard');
+//})->middleware(['auth'])->name('dashboard');
 
 // プロジェクト
-//Route::get('/', [App\Http\Controllers\ProjectController::class, 'index'])->middleware(['auth']);
-Route::resource('project', ProjectController::class)->
-       except(['destroy'])->
-       middleware(['auth']);
+Route::get('/', [App\Http\Controllers\ProjectController::class, 'index'])
+     ->name('/')
+     ->middleware(['auth']);
+Route::resource('project', ProjectController::class)
+     ->except(['destroy'])
+     ->middleware(['auth']);
 // プロジェクト詳細
-Route::resource('project.project_detail', ProjectDetailController::class, ['names' => 'project_detail'])->
-       parameters(['project' => 'project_id', 'project_detail' => 'id'])->
-       except(['show', 'destroy'])->
-       middleware(['auth']);
+Route::resource('project.project_detail', ProjectDetailController::class, ['names' => 'project_detail'])
+     ->parameters(['project' => 'project_id', 'project_detail' => 'id'])
+     ->except(['show', 'destroy'])
+     ->middleware(['auth']);
 // 発注者
-Route::resource('orderer', OrdererController::class)->
-       except(['show', 'destroy'])->
-       middleware(['auth']);
+Route::resource('orderer', OrdererController::class)
+     ->except(['show', 'destroy'])
+     ->middleware(['auth']);
 // クラウドソーシング
-Route::resource('crowd_sourcing', CrowdSourcingController::class)->
-       except(['show', 'destroy'])->
-       middleware(['auth']);
+Route::resource('crowd_sourcing', CrowdSourcingController::class)
+     ->except(['show', 'destroy'])
+     ->middleware(['auth']);
 // 進捗
-Route::resource('progress', ProgressController::class)->
-       except(['show', 'destroy'])->
-       middleware(['auth']);
+Route::resource('progress', ProgressController::class)
+     ->except(['show', 'destroy'])
+     ->middleware(['auth']);
 
 require __DIR__.'/auth.php';
